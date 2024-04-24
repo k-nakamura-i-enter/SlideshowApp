4f6125bf647ec7ba023ee80668af5ac7eacbee39
nakanakamuramura <k-nakamura@i-enter.co.jp>
2024-04-24 16:01:06 +0900

    非対称性の改善

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     }
     var imageIndex = 0
     var timer: Timer!
-    var mode: Bool = false
     
     let images = [
         UIImage(named: "TA課題用1.png"),
@@ -38,19 +37,18 @@ class ViewController: UIViewController {
     }
     //スライドショーのとき呼び出されるもの
     @objc func updateSlide(_ timer: Timer){
-        imageIndex = (imageIndex + 1) % images.count
-        imageView.image = images[imageIndex]
+        nextImage()
     }
     //画像タッチで呼び出されるもの
     @objc func imageTapped() {
-        if mode {
+        if self.timer != nil {
             self.timer.invalidate()
         }
         performSegue(withIdentifier: "toNext", sender: nil)
     }
     //戻った時
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
-        if mode {
+        if self.timer != nil {
             self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateSlide(_:)), userInfo: nil, repeats: true)
         }
     }
@@ -59,26 +57,20 @@ class ViewController: UIViewController {
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
         resultViewController.images = images[imageIndex]
     }
+    
     //次へ
     @IBAction func nextSlide(_ sender: Any) {
-        imageIndex = (imageIndex + 1) % images.count
-        imageView.image = images[imageIndex]
+        nextImage()
     }
     //前へ
     @IBAction func previousSlide(_ sender: Any) {
-        if  imageIndex > 0{
-            imageIndex = (imageIndex - 1) % images.count
-        }
-        else{
-            imageIndex = 2
-        }
+        imageIndex = (imageIndex - 1 + images.count) % images.count
         imageView.image = images[imageIndex]
     }
     //スライドショー
     @IBAction func startSlideshow(_ sender: Any) {
         nextButton.isEnabled = !nextButton.isEnabled
         preButton.isEnabled = !preButton.isEnabled
-        mode = !mode
         if self.timer == nil {
             startButton.setTitle("停止", for: .normal)
             self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateSlide(_:)), userInfo: nil, repeats: true)
@@ -88,6 +80,12 @@ class ViewController: UIViewController {
             self.timer.invalidate()   // タイマーを停止する
             self.timer = nil
         }
+    }
+    
+    //次へメソッド
+    func nextImage(){
+        imageIndex = (imageIndex + 1 + images.count) % images.count
+        imageView.image = images[imageIndex]
     }
 }
 
